361d77d556d9f643ac6fb6f05f0b01ba93554f03
David Berkman <david.berkman@grenadefish.net>
2020-12-09 15:30:12 -0800

    with copywrite

--- api/src/main/java/com/forio/epicenter/model/api/GroupMandate.java
+++ api/src/main/java/com/forio/epicenter/model/api/GroupMandate.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 Forio
+ * 
+ * This file is part of the Forio Epicenter project.
+ * 
+ * These files from the Forio Epicenter project are free software, you can
+ * redistribute and/or modify them under the terms of the Apache License, Version 2.0.
+ * 
+ * These files from the Forio Epicenter project are distributed in the hope that
+ * they will be useful, but are WITHOUT ANY WARRANTY; without even an implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Apache License
+ * for more details.
+ * 
+ * You should have received a copy of the Apache License along with the these Forio
+ * Epicenter project files. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
 package com.forio.epicenter.model.api;
 
 public enum GroupMandate {
