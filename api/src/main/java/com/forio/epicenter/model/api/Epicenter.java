709aae58a7bce15e2f318618b0bcadba909aee87
David Berkman <david.berkman@grenadefish.net>
2020-12-14 14:52:48 -0800

    thread priv actions

--- api/src/main/java/com/forio/epicenter/model/api/Epicenter.java
+++ api/src/main/java/com/forio/epicenter/model/api/Epicenter.java
@@ -1,21 +1,22 @@
 /*
  * Copyright (c) 2020 Forio
- * 
+ *
  * This file is part of the Forio Epicenter project.
- * 
+ *
  * These files from the Forio Epicenter project are free software, you can
  * redistribute and/or modify them under the terms of the Apache License, Version 2.0.
- * 
+ *
  * These files from the Forio Epicenter project are distributed in the hope that
  * they will be useful, but are WITHOUT ANY WARRANTY; without even an implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Apache License
  * for more details.
- * 
+ *
  * You should have received a copy of the Apache License along with the these Forio
  * Epicenter project files. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
  */
 package com.forio.epicenter.model.api;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Consumer;
 
 public class Epicenter {
@@ -25,6 +26,21 @@ public class Epicenter {
   public static void setCurrent (EpicenterAccessible epicenterAccessible) {
 
     epicenterAccessibleLocal.set(epicenterAccessible);
+  }
+
+  public static Thread setContextClassLoader (Thread thread) {
+
+    return epicenterAccessibleLocal.get().setContextClassLoader(thread);
+  }
+
+  public static ForkJoinPool constructForkJoinPool (int parallelism) {
+
+    return epicenterAccessibleLocal.get().constructForkJoinPool(parallelism);
+  }
+
+  public static Actor getActor () {
+
+    return epicenterAccessibleLocal.get().getActor();
   }
 
   public static void subscribe (String name, Consumer<Object[]> consumer) {
--- api/src/main/java/com/forio/epicenter/model/api/EpicenterAccessible.java
+++ api/src/main/java/com/forio/epicenter/model/api/EpicenterAccessible.java
@@ -1,24 +1,29 @@
 /*
  * Copyright (c) 2020 Forio
- * 
+ *
  * This file is part of the Forio Epicenter project.
- * 
+ *
  * These files from the Forio Epicenter project are free software, you can
  * redistribute and/or modify them under the terms of the Apache License, Version 2.0.
- * 
+ *
  * These files from the Forio Epicenter project are distributed in the hope that
  * they will be useful, but are WITHOUT ANY WARRANTY; without even an implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Apache License
  * for more details.
- * 
+ *
  * You should have received a copy of the Apache License along with the these Forio
  * Epicenter project files. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
  */
 package com.forio.epicenter.model.api;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.function.Consumer;
 
 public interface EpicenterAccessible {
+
+  Thread setContextClassLoader (Thread thread);
+
+  ForkJoinPool constructForkJoinPool (int parallelism);
 
   Actor getActor ();
 
