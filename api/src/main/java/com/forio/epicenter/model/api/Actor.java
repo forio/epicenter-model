fdce54352af569e912615619c96b64e56319965b
David Berkman <david.berkman@grenadefish.net>
2022-04-07 02:21:28 -0700

    jakarta jaxb

--- api/src/main/java/com/forio/epicenter/model/api/Actor.java
+++ api/src/main/java/com/forio/epicenter/model/api/Actor.java
@@ -1,26 +1,26 @@
 /*
  * Copyright (c) 2020 Forio
- * 
+ *
  * This file is part of the Forio Epicenter project.
- * 
+ *
  * These files from the Forio Epicenter project are free software, you can
  * redistribute and/or modify them under the terms of the Apache License, Version 2.0.
- * 
+ *
  * These files from the Forio Epicenter project are distributed in the hope that
  * they will be useful, but are WITHOUT ANY WARRANTY; without even an implied warranty
  * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Apache License
  * for more details.
- * 
+ *
  * You should have received a copy of the Apache License along with the these Forio
  * Epicenter project files. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
  */
 package com.forio.epicenter.model.api;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import org.graalvm.polyglot.HostAccess;
 
 @XmlRootElement(name = "actor")
